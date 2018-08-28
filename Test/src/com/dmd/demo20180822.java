c0870a0b06cdece263c25f057897cdf5a44a8430
qirongxuDMD <15062872214@163.com>
2018-08-29 00:06:05 +0800

    修改名字

--- Test/src/com/dmd/demo20180822.java
+++ Test/src/com/dmd/demo20180822.java
@@ -3,10 +3,12 @@ package com.dmd;
 public class demo20180822 {
 	public static void main(String[] args) {
 		Person person1 = new Person();
-//		person.name = "linda";
-//		person.age = 23;
+		person1.age = 23;
+		person1.name = "linda";
 //		person.info();
-		Person person2=person1;
+		Person person2 = person1;
+		person2.name = "jack";
+		person2.info();
 	}
 }
 
